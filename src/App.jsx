d541ba648a4859a88ac846df60ffc0c6e851a633
naziya <naaziyapatel36@gmail.com>
2023-08-11 14:19:04 +0530

    payment page imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Payment from './Components/Payments/Payment'
-// import PaymentsPage from './PaymentsPage'
 
 function App() {
   return (
     <div>
-      {/* <PaymentsPage/> */}
       <Payment/>
     </div>
   )
